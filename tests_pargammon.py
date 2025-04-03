c01440c5d27a3841f558dc1c2bd5525c5e2bf356
gek0 <angelrojosanz06@gmail.com>
2025-04-03 02:03:49 +0200

    Maquina L \ Maquina T \ Humano

--- tests_pargammon.py
+++ tests_pargammon.py
@@ -1,9 +1,9 @@
 #####################
 ##    PARGAMMON    ##
-## ANGEL ROJO SANZ ##
+## ÁNGEL ROJO SANZ ##
 #####################
 
-from random import randrange, seed, getstate, setstate
+from random import randrange, seed, choice
 
 AZAR = 75 # Semilla para el random
 
@@ -33,6 +33,19 @@ class Pargamon(object):
         self.JUGADAS_POSIBLES = []
         self.PUNTUACIONES = [0] * len(fichas)
         self.FICHAS_SACADAS = [0] * len(fichas)
+        self.FICHAS_SACADAS_SIM = [0] * len(fichas)
+        
+        self.MAQUINAS_LISTAS = ()
+        self.MAQUINAS_TONTAS = ()
+
+
+        for i in range(len(self.FICHAS)):
+            tipo = input(f"Jugador {self.FICHAS[i]} es [H]umano, Máquina [T]onta o Máquina [L]ista: ")
+            if tipo == 'T':
+                self.MAQUINAS_TONTAS = self.MAQUINAS_TONTAS + (i,)
+            elif tipo == 'L':
+                self.MAQUINAS_LISTAS = self.MAQUINAS_LISTAS + (i,)
+        
         
         # Agrega N columnas vacías al tablero
         for i in range(self.N):
@@ -65,6 +78,7 @@ class Pargamon(object):
 
 
     def cambiar_turno(self) -> bool:
+        
         # Comprueba si el jugador ha sacado todas las fichas del tablero
         if self.FICHAS_SACADAS[self.TURNO] == self.M:
             print(f"Han ganado los {self.FICHAS[self.TURNO]}!")
@@ -73,35 +87,57 @@ class Pargamon(object):
         self.JUGADAS += 1
         self.TURNO = (self.JUGADAS-1) % len(self.FICHAS)
         self.actualziarPuntos()
+        # Guarda el estado actual para poder deshacer
+        self.guardarEstado()
+
+        # Actualiza y ordena la lista de posibles jugadas
+        self.JUGADAS_POSIBLES.clear()
         self.buscarJugadas(0, self.TABLERO)
+        self.JUGADAS_POSIBLES.sort(reverse=True)
+        if self.TURNO in self.MAQUINAS_LISTAS:
+            print(self)
+            movimiento = self.JUGADAS_POSIBLES[0].MOVS 
+            print("Jugada: ", movimiento)
+            self.jugar(movimiento)
+            return self.cambiar_turno()
+        elif self.TURNO in self.MAQUINAS_TONTAS:
+            print(self)
+            movimiento = choice(self.JUGADAS_POSIBLES).MOVS 
+            print("Jugada: ", movimiento)
+            self.jugar(movimiento)
+            return self.cambiar_turno()
         return False
     
 
     def jugar(self, txt_jugada: str) -> None | str:
-        if len(txt_jugada) > self.D or len(txt_jugada) < self.D:
+        if txt_jugada[0] == '*':
+            self.deshacer(len(txt_jugada))
+            return None
+        elif len(txt_jugada) > self.D or len(txt_jugada) < self.D:
             return f'ERROR J1: Debe indicar exactamente {self.D} movimientos.'
         else:
             movimientos_invalidos = []
             tablero_simulado = self.copiarTablero(self.TABLERO)
             for i in range(self.D):
-                print("Tablero SIM: \n", tablero_simulado)
                 char = txt_jugada[i]
                 if char == '@': continue
                 saltos = self.dados[i]
                 col = ord(char) - 65
-                if col > self.N:
+                if col >= self.N:
                     movimientos_invalidos.append(char)
                     continue
-                if col + saltos > self.N:
-                    return(f"ERROR J2-M3: Movimiento {char} -> {chr(col + saltos + 65)}, columna destino fuera de rango.")
-
-                print("Longitud destino: ",len(tablero_simulado[col+saltos]))
-                if len(tablero_simulado[col+saltos]) > 1:
-                    return(f"ERROR J2-M4: Movimiento {char} -> {chr(col + saltos + 65)}, tiene más de una ficha contraria.")
-
-                self.moverFicha(col, col+saltos, tablero_simulado)
             if len(movimientos_invalidos) > 0:
+                movimientos_invalidos.sort(reverse=1)
                 return f"ERROR J2-M1: No existen columna(s) con estas letras: {",".join(movimientos_invalidos)}."
+            for i in range(self.D):
+                char = txt_jugada[i]
+                if char == '@': continue
+                saltos = self.dados[i]
+                col = ord(char) - 65
+                validacion = self.validarJugada(col, saltos, tablero_simulado)
+                if validacion != True:
+                    return validacion
+                self.moverFicha(col, col+saltos, tablero_simulado)
             else:
                 for i in range(self.D):
                     char = txt_jugada[i]
@@ -109,42 +145,38 @@ class Pargamon(object):
                     saltos = self.dados[i]
                     col = ord(char) - 65
                     self.moverFicha(col, col+saltos, self.TABLERO)
-            print(movimientos_invalidos)
             return None
 
 
-    def validarJugada(self, colI, saltos):
+    def validarJugada(self, colI, saltos, tablero):
         colF = colI + saltos
-        print("COLF: ", colF)
         ficha_turno = self.FICHAS[self.TURNO]
-        if colI > self.N | colI < 0:
-            print("Fuera de rango")
-            return False
-        elif len(self.TABLERO[colI]) == 0:
-            print("Vacío")
-            return False
-        elif self.TABLERO[colI][0] != ficha_turno:
-            print("No tiene fichas del jugador")
-            return False
+        if colI >= self.N | colI < 0:
+            return "ERROR: Columna de origen fuera de rango."
+        elif len(tablero[colI]) == 0:
+            return f"ERROR J2-M2: Columna de origen {chr(colI + 65)} no tiene fichas del jugador"
+        elif tablero[colI][0] != ficha_turno:
+            return f"ERROR J2-M4: Movimiento {chr(colI + 65)} -> {chr(colF + 65)}, columna destino tiene más de una ficha contraria."
         elif colF > self.N:
-            print("Salto fuera del tablero")
-            return False
+            return f"ERROR J2-M3: Movimiento {chr(colI + 65)} -> {chr(colF + 65)}, columna destino fuera de rango."
         elif colF == self.N:
             return True
-        elif len(self.TABLERO[colF]) > 1:
-            if self.TABLERO[colF][0] != ficha_turno:
-                print("Ocupado")
-                return False
+        elif len(tablero[colF]) > 1:
+            if tablero[colF][0] != ficha_turno:
+                return "ERROR: Columna destino ocupada"
         else:
             return True
 
-    def moverFicha(self, ind_colI, ind_colF, tablero):
+    def moverFicha(self, ind_colI, ind_colF, tablero, simulacion = False):
         try:
             colI = tablero[ind_colI]
 
             if ind_colF == self.N:
                 colI.pop()
-                self.FICHAS_SACADAS[self.TURNO] += 1
+                if simulacion == True:
+                    self.FICHAS_SACADAS_SIM[self.TURNO] +=1
+                else:
+                    self.FICHAS_SACADAS[self.TURNO] += 1
                 return
           
             colF = tablero[ind_colF]
@@ -162,9 +194,12 @@ class Pargamon(object):
             return e
 
     
-    def calcularPuntos(self, tablero, jugador): # Jugador // Turno [0, n-1 jugadores]
+    def calcularPuntos(self, tablero, jugador, simulacion = False): # Jugador // Turno [0, n-1 jugadores]
         puntos = 0
-        puntos += (3 * (self.N+1) * (self.FICHAS_SACADAS[jugador]))
+        if simulacion == True:
+            puntos += (3 * (self.N+1) * (self.FICHAS_SACADAS_SIM[jugador]))
+        else:
+            puntos += (3 * (self.N+1) * (self.FICHAS_SACADAS[jugador]))
         for col_i in range(self.N):
             col = tablero[col_i]
             nC = col.count(self.FICHAS[jugador])
@@ -220,29 +255,29 @@ class Pargamon(object):
             self.JUGADAS = jugadas
             self.TURNO = turno
             print(f"Se ha retrocedio a la jugada #{self.JUGADAS}.")
-            
         else:
             print("ERROR: No se ha podido recuperar el estado.")
-        return
+        return None
 
 
     def buscarJugadas(self, n_dado, tablero, txt_jugadas = ""):
-        print("Buscando jugada: ", n_dado)
         puntuacion = 0
         if n_dado == 0:
             tablero_sim = self.copiarTablero(self.TABLERO)
         else:
             tablero_sim = tablero
+
         if n_dado == self.D:
-            puntuacion = (self.calcularPuntos(tablero_sim, self.TURNO) - self.calcularPuntos(self.TABLERO, self.TURNO))
+            puntuacion = (self.calcularPuntos(tablero_sim, self.TURNO, True) - self.calcularPuntos(self.TABLERO, self.TURNO))
             self.JUGADAS_POSIBLES.append(Jugada(txt_jugadas, puntuacion))
+            self.FICHAS_SACADAS_SIM = list(fichas for fichas in self.FICHAS_SACADAS)
             return
         
         dado = self.dados[n_dado]
         for i_col in range(self.N):
-            if self.validarJugada(i_col, dado):
+            if self.validarJugada(i_col, dado, tablero_sim) == True:
                 tablero_temp = self.copiarTablero(tablero_sim)
-                self.moverFicha(i_col, i_col + dado, tablero_temp)
+                self.moverFicha(i_col, i_col + dado, tablero_temp, simulacion=True)
                 self.buscarJugadas(n_dado + 1, tablero_temp, txt_jugadas + chr(i_col + 65))
         
         self.buscarJugadas(n_dado + 1, tablero_sim, txt_jugadas + '@')
@@ -252,14 +287,14 @@ def main():
     seed(AZAR)
     print("*** PARGAMMON ***")
     #params = map(int, input("Numero de columnas, fichas y dados = ").split())
-    juego = Pargamon(*[10, 5,3])
+    juego = Pargamon(*[12,5,4])
     finPartida = juego.cambiar_turno()
     print(juego)
-    while not finPartida:        
-        jugada = juego.jugar(input("Introduce tu jugada: "))
+    while not finPartida:
+        jugada = juego.jugar(input("Jugada: "))
         while jugada != None:
             print(jugada)
-            jugada = juego.jugar(input("Introduce tu jugada: "))
+            jugada = juego.jugar(input("Jugada: "))
         finPartida = juego.cambiar_turno()
         print(juego)
 main()
